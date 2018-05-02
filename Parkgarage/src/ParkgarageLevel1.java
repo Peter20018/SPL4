ceb4f9a70dadafefd09d9c99d5a6878a10f99da1
Peter20018 <171witb10@o365.htl-leoben.at>
2018-05-02 09:33:12 +0200

    Übung 1 erweitert

--- Parkgarage/src/ParkgarageLevel1.java
+++ Parkgarage/src/ParkgarageLevel1.java
@@ -3,10 +3,10 @@ public class ParkgarageLevel1 {
 
 	public static void main(String[] args) {
 		
-		int n = 10;
-		int autos = 4;
+		int n = 100;
+		int autos = 20;
 		
-		String parken = "6 2 -6 3 4 -3 -4 -2";
+		String parken = "19 -19 11 -11 1 -1 17 -17 10 7 -10 -7 3 14 -14 -3 13 -13 4 12 20 18 6 16 -18 -12 -16 5 -5 -20 -6 -4 8 15 -8 2 -15 9 -2 -9";
 		String[] parkvorgaenge = parken.split(" ");
 		
 		int autosInGarage = 0; 
@@ -28,9 +28,12 @@ public class ParkgarageLevel1 {
 			{
 				System.out.println("Irgendetwas hat nicht funktioniert. ");
 			}
-			
+			if(maxAutos < autosInGarage) {
+				maxAutos = autosInGarage;
+			}
 			System.out.println("So viele Autos sind zurzeit in der Garage " + autosInGarage);
 		}
-		
+		System.out.println("Maximale Anzahl an Autos: " + maxAutos);
+		}
 	}
-}
+
